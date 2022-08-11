f7fbc2cfc96e28b963ab78f7751a087daf7213a7
Vladimir LABUNETS <vplabunets@gmail.com>
2022-08-11 08:02:34 +0300

    10 08  2022

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import * as basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
 
-console.log(galleryItems);
+basicLightbox
+  .create(
+    `
+		<img width="1400" height="900" src="https://placehold.it/1400x900">
+	`
+  )
+  .show();
+
+console.log(instance);
+instance.show();
+const imagesList = document.querySelector(".gallery");
+const ccc = addEventListener("click", getOriginalUrl);
+function getOriginalUrl(event) {
+  let originalUrl = "";
+  //   console.log(event.target.alt);
+  galleryItems.forEach((image) => {
+    // console.log(image.description);
+    // console.log(event.target.alt);
+    if (image.description === event.target.alt) {
+      originalUrl = image.original;
+    }
+  });
+
+  console.log(originalUrl);
+  return originalUrl;
+}
+
+const ulList = galleryItems
+  .map(
+    (image) =>
+      `<li class = 'imageitem'><img src ='${image.preview}' alt = '${image.description}'></li>`
+  )
+  .join("");
+console.log(imagesList);
+imagesList.insertAdjacentHTML("afterbegin", ulList);
