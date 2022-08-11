37e64fc0f6873d8486318498e2d15c8045e88467
Vladimir LABUNETS <vplabunets@gmail.com>
2022-08-11 21:22:15 +0300

    1 exercise done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,39 +1,73 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-import * as basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
 
-basicLightbox
-  .create(
-    `
-		<img width="1400" height="900" src="https://placehold.it/1400x900">
-	`
-  )
-  .show();
-
-console.log(instance);
-instance.show();
 const imagesList = document.querySelector(".gallery");
-const ccc = addEventListener("click", getOriginalUrl);
-function getOriginalUrl(event) {
-  let originalUrl = "";
-  //   console.log(event.target.alt);
-  galleryItems.forEach((image) => {
-    // console.log(image.description);
-    // console.log(event.target.alt);
-    if (image.description === event.target.alt) {
-      originalUrl = image.original;
-    }
-  });
-
-  console.log(originalUrl);
-  return originalUrl;
-}
-
+// console.log(imagesList);
 const ulList = galleryItems
   .map(
-    (image) =>
-      `<li class = 'imageitem'><img src ='${image.preview}' alt = '${image.description}'></li>`
+    (image) => `<div class="gallery__item">
+  <a  id = "test" class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
   )
   .join("");
-console.log(imagesList);
+
+// Adding markup for gallery
 imagesList.insertAdjacentHTML("afterbegin", ulList);
+
+//Element search in DOM
+const imagesListEL = document.querySelector(".gallery");
+//Adding listener
+imagesListEL.addEventListener("click", getOriginalUrl);
+function getOriginalUrl(event) {
+  //Checking of targeting to gallery item element
+  if (event.target === event.currentTarget) return;
+  //Link openning prevention
+  event.preventDefault();
+  //Assignment of large image url
+  const originalUrl = event.target.dataset.source;
+  // Modal menu creation
+  const modal = basicLightbox.create(
+    `<div class="modal">
+  	<img width="1400" height="900" src="${originalUrl}"></div>
+  `
+  );
+  modal.show();
+  // Modal closure by pushing Escape
+  document.addEventListener("keydown", function (event) {
+    // Escape pushing checking
+    if (event.key === "Escape") {
+      // Modal close call
+      modal.close();
+    }
+  });
+}
+
+// const imagesListEL = document.querySelector(".gallery");
+// //Adding listener
+// imagesListEL.addEventListener("click", getOriginalUrl);
+// function getOriginalUrl(event) {
+//   console.log(event.target.alt);
+//   galleryItems.forEach((image) => {
+//     let originalUrl = "";
+//     event.preventDefault();
+//     if (image.description === event.target.alt) {
+//       originalUrl = image.original;
+//       basicLightbox
+//         .create(
+//           `<div class="modal">
+//   	<img width="1400" height="900" src="${originalUrl}"></div>
+//   `
+//         )
+//         .show();
+//       return;
+//     }
+//     return;
+//   });
+// }
